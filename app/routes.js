56eeb3ed9d5bbb28ad32197c20c0476a35bc761f
swathi <rswat@seas.upenn.edu>
2017-04-22 20:07:22 -0400

    Final without playlist update

--- app/routes.js
+++ app/routes.js
@@ -18,23 +18,14 @@ connection.query('USE ' + dbconfig.database);
 
 module.exports = function(app, passport) {
 
-	app.get('/', function(req, res) {
-
-/**		var getquery = "set @var = (select id1 from (select at1.artist_id as id1, at2.artist_id as id2, count(*) from Artist_Track at1 join Artist_Track at2 on at1.track_id=at2.track_id where at1.artist_id != at2.artist_id group by at1.artist_id, at2.artist_id order by count(*) desc) as T limit 1); set @var1 = (select id2 from (select at1.artist_id as id1, at2.artist_id as id2, count(*) from Artist_Track at1 join Artist_Track at2 on at1.track_id=at2.track_id where at1.artist_id != at2.artist_id group by at1.artist_id, at2.artist_id order by count(*) desc) as T limit 1); select name, Spotify_url from Track where id in (select AT1.track_id from Artist_Track AT1 where AT1.artist_id = @var and AT1.track_id in (select AT2.track_id from Artist_Track AT2 where AT2.artist_id = @var1))"**/
+	app.get('/', isNotLoggedIn, function(req, res) {
 		var getquery = "select name, Spotify_url from Track where id in (select AT1.track_id from Artist_Track AT1 where AT1.artist_id = (select id1 from (select at1.artist_id as id1, at2.artist_id as id2, count(*) from Artist_Track at1 join Artist_Track at2 on at1.track_id=at2.track_id where at1.artist_id != at2.artist_id group by at1.artist_id, at2.artist_id order by count(*) desc) as T limit 1) and AT1.track_id in (select AT2.track_id from Artist_Track AT2 where AT2.artist_id = (select id2 from (select at1.artist_id as id1, at2.artist_id as id2, count(*) from Artist_Track at1 join Artist_Track at2 on at1.track_id=at2.track_id where at1.artist_id != at2.artist_id group by at1.artist_id, at2.artist_id order by count(*) desc) as T limit 1)))";
 		connection.query(getquery, function(err, rows) {
 			if (err)
 				throw err;
-			var data=[];
-			for(i=0;i<rows.length;i++){
-							data.push(rows);
-			}
-//console.log(rows);
 			res.render('index.ejs',{sql:rows});
 		});
 	});
-
-
 
 
 	app.post('/', function(req, res) {
@@ -90,9 +81,28 @@ module.exports = function(app, passport) {
 	}));
 
 	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user
+		var getquery = "select name, Spotify_url from Track where id in (select AT1.track_id from Artist_Track AT1 where AT1.artist_id = (select id1 from (select at1.artist_id as id1, at2.artist_id as id2, count(*) from Artist_Track at1 join Artist_Track at2 on at1.track_id=at2.track_id where at1.artist_id != at2.artist_id group by at1.artist_id, at2.artist_id order by count(*) desc) as T limit 1) and AT1.track_id in (select AT2.track_id from Artist_Track AT2 where AT2.artist_id = (select id2 from (select at1.artist_id as id1, at2.artist_id as id2, count(*) from Artist_Track at1 join Artist_Track at2 on at1.track_id=at2.track_id where at1.artist_id != at2.artist_id group by at1.artist_id, at2.artist_id order by count(*) desc) as T limit 1)))";
+		connection.query(getquery, function(err, rows) {
+			if (err)
+				throw err;
+			res.render('profile.ejs',{sql:rows , user : req.user});
 		});
+	});
+
+
+
+
+	app.post('/profile', function(req, res) {
+        	var decade = req.body.decade;
+		var getquery = "select name, Spotify_url from Track T1 inner join (select id from (select t.id, count(*) from Track t join Favorites f on t.id = f.track_id where t.year between ? and ? group by t.id order by count(*) desc)p) as T on T1.id = T.id limit 3"
+			connection.query(getquery,[Number(decade), Number(decade)+9], function(err, rows) {
+				if (err)
+					throw err;
+				if(rows.length>0)
+					res.render('profile_results.ejs',{sql:rows, message:'' , user : req.user});
+				else
+					res.render('profile_results.ejs',{sql:rows, message:'None Found' , user : req.user});
+			});
 	});
 
 	app.get('/account', isLoggedIn, function(req, res) {
@@ -107,101 +117,180 @@ module.exports = function(app, passport) {
 		res.redirect('/');
 	});
 
-	app.get('/display', function(req, res) {
+	app.get('/display', isLoggedIn, function(req, res) {
 		res.render('display.ejs', { message: req.flash('loginMessage') });
 	});
 
-	app.post('/display', function(req, res) {
+	app.post('/display',  isLoggedIn, function(req, res) {
         	var query = req.body.tag
 		console.log(query);
 		var getquery = "SELECT * from "+query;
 			connection.query(getquery, function(err, rows) {
 				if (err)
 					return done(err);
-				var data=[];
-				for(i=0;i<rows.length;i++)
-					{
-					data.push(rows[i]);
-					}
 				res.render('display_results.ejs', {
 					sql : rows
 				});
 			});
 	});
 
-	app.get('/search', function(req, res) {
+	app.get('/search', isLoggedIn, function(req, res) {
 		res.render('search.ejs', { message: req.flash('loginMessage') });
 	});
 
-	app.get('/favourites', function(req, res){
-		res.render('favourite_results.ejs', { message: req.flash('loginMessage') });
-	});
-
-	app.post('/favourites', function(req, res){
-        var query = req.body.favourite;		
+	app.get('/favourites',isLoggedIn, function(req, res){
 	var user = req.user.username;
-		var msg = '';
-
-		var firstquery = "select * from Favorites where user_id = "+user+" and track_id = "+query;
-		connection.query(firstquery, function(err, rows) {
-			if (rows.length){
-				msg = 'Songs already in favourites'
-				res.render('favourite_results.ejs',{sql:rows, message: msg});
-			}
-			else{
-
-
-			var getquery = "insert into Favorites (user_id, track_id) values ("+user+","+query+")";
-					connection.query(getquery, function(err, result) {
-						if (err){
-							console.log(err);
-						}
-					});
-
-			var getquery = "select * from Track where id in (SELECT track_id from Favorites where user_id = "+user+")";
+	var getquery = "select * from Track where id in (SELECT track_id from Favorites where user_id = "+user+")";
 				connection.query(getquery, function(err, rows) {
 					if (err)
 						throw err;
-					res.render('favourite_results.ejs',{sql:rows, message:''});
+					if(rows.length>0){
+						res.render('favourite_results.ejs', {
+							sql : rows, message: ''
+						});
+					}
+					else {
+						res.render('favourite_results.ejs', {
+							sql : rows, message: 'None Found'
+						});
+					}
 				});
-
-			}
-		});
-
 	});
 
-	app.post('/search', function(req, res) {
+	app.post('/favourites',isLoggedIn, function(req, res){
+        var query = req.body.favourite;	
+	var not = req.body.notfavourite;		
+	var user = req.user.username;
+		var msg = '';
+
+	if (typeof not !== 'undefined' && not !== null){
+
+var getquery = "delete from Favorites where user_id = "+user+" and track_id = "+not;
+
+			connection.query(getquery, function(err, rows) {
+				if (err){
+							console.log(err);
+						}
+						});
+
+				var getquery = "select * from Track where id in (SELECT track_id from Favorites where user_id = "+user+")";
+					connection.query(getquery, function(err, rows) {
+						if (err)
+							throw err;
+						res.render('favourite_results.ejs',{sql:rows, message:''});
+					});
+	}
+
+	else
+	{	var firstquery = "select * from Favorites where user_id = "+user+" and track_id = "+query;
+			connection.query(firstquery, function(err, rows) {
+				if (rows.length){
+					msg = 'Songs already in favourites'
+					res.render('favourite_results.ejs',{sql:rows, message: msg});
+				}
+				else{
+
+
+				var getquery = "insert into Favorites (user_id, track_id) values ("+user+","+query+")";
+						connection.query(getquery, function(err, result) {
+							if (err){
+								console.log(err);
+							}
+						});
+
+				var getquery = "select * from Track where id in (SELECT track_id from Favorites where user_id = "+user+")";
+					connection.query(getquery, function(err, rows) {
+						if (err)
+							throw err;
+						res.render('favourite_results.ejs',{sql:rows, message:''});
+					});
+
+				}
+			});
+
+		}
+	});
+
+	app.post('/search',  isLoggedIn, function(req, res) {
         var query = req.body.query,
-		table = req.body.tag;
-		console.log(table);
+	artist = req.body.artist,
+	album = req.body.album,	
+	table = req.body.tag;
+		
+	if (typeof artist !== 'undefined' && artist !== null){
+		var getquery = "select name,theme,duration,year,Spotify_url from Track t where id in ( select track_id from (select at.track_id, count(*) from Artist_Track at join Favorites F on at.track_id = F.track_id where at.artist_id in (Select id from Artist where name like ?) group by at.track_id order by count(*)) ft) limit 3";
+			connection.query(getquery,["%"+artist+"%"], function(err, rows) {
+				if (err)
+					throw err;
+				if(rows.length>0){
+					res.render('search_results.ejs', {
+						sql : rows, message: ''
+					});
+				}
+				else {
+					res.render('search_results.ejs', {
+						sql : rows, message: 'None Found'
+					});
+				}
+			});
+	}
+
+	else if (typeof album !== 'undefined' && album !== null){
+
+		var getquery = "select t.name,at.name from ((select id from Album where name = ?) ab Join (select id,name,album_id from Track) t on t.album_id= ab.id join Artist_Track trat on trat.track_id=t.id Join Artist at on at.id= trat.artist_id) order by t.name";
+
+			connection.query(getquery,[album], function(err, rows) {
+				if (err)
+					throw err;
+				if(rows.length>0){
+					res.render('search_results.ejs', {
+						sql : rows, message: ''
+					});
+				}
+				else{
+					res.render('search_results.ejs', {
+						sql : rows, message: 'None Found'
+					});
+				}
+			});
+
+	}
+
+	else {
 		var getquery = "SELECT * from "+table+" where name like ?";
-		if(table == 'Artist')
-			getquery = "select * from Track t where id in (select track_id from (select at.track_id, count(*) from Artist_Track at join Favorites F on at.track_id = F.track_id where artist_id = (Select artist_id from Artist where name like ? group by at.track_id order by count(*))) temp)";
 			connection.query(getquery,[query+"%"], function(err, rows) {
 				if (err)
 					throw err;
 				res.render('search_results.ejs', {
-					sql : rows
+					sql : rows, message: ''
 				});
 			});
+		}
 	});
+
 	var temp;
 	///////////////////RRRR/////////////////
 	app.get('/playlist', function(req, res) {
-		 console.log(0);
+		 //console.log(0);
 		 mongodb.connect(database_mongo.url,function(err, db){
-		 	console.log(1);
+		 	//console.log(1);
 		 db.collection('Collection_user', function (err, collection) {
-		 	console.log(2);
-         collection.find({user_id: req.user.username}).toArray(function(err, items) {
-            if(err) throw err;    
-            temp=items;
-            
-            console.log("Done");
-            // alert("Done!"); 
-		res.render('playlist.ejs', { items: temp, message: req.flash('loginMessage') }); 
-                 
-        });
+		 	//console.log(2);
+	try{
+		 collection.find({user_id: req.user.username}).toArray(function(err, items) {
+		    if(err) throw err;    
+		    temp=items;
+		    
+		    //console.log("Done");
+		    // alert("Done!"); 
+			res.render('playlist.ejs', { items: temp, message: req.flash('loginMessage') }); 
+		         
+		});
+	}
+
+	catch(err){
+		res.redirect('/');
+	}
      	});
 		// db.close();
 		});	
@@ -316,10 +405,10 @@ module.exports = function(app, passport) {
 
 			var cursor2=db.collection('Collection_user').find({user_id: {$in: resultArray1}},{song_id:1, _id:0});
 		 	cursor2.forEach(function(doc,error){
-		 	console.log("inside function");
+		 	//console.log("inside function");
 		 	
 		 	for(i=0;i<doc['song_id'].length;i++){resultArray2.push(doc['song_id'][i])}
-			console.log(resultArray2);
+			//console.log(resultArray2);
 	     
 		 });
 		});
@@ -338,7 +427,7 @@ module.exports = function(app, passport) {
 					}
 				// res.end(JSON.stringify(data));
 				// res.render('playlist.ejs');
-				console.log("Done");
+				//console.log("Done");
 				
 				resultArray3.push(resultArray2[0]);
 		 		for(k=0;k<resultArray2.length;k++){
@@ -348,11 +437,11 @@ module.exports = function(app, passport) {
 			 		}
 			 		if(ctr==0){resultArray3.push(resultArray2[k])}
 			 	}
-			 console.log(resultArray3[0])
+			// console.log(resultArray3[0])
 			 if((resultArray3[0])==undefined){
-			 	console.log("undef");
+			 	//console.log("undef");
 			 	resultArray3=["No recommendations"];}
-			 console.log(resultArray3);
+			// console.log(resultArray3);
 				res.render('add_songs.ejs', { items: resultArray, items1: resultArray1, items2: resultArray3, query : temp1, list: rows, message: req.flash('loginMessage') });
 			});
 		// res.render('profile.ejs', {
@@ -365,26 +454,25 @@ module.exports = function(app, passport) {
 
 
 	app.post('/add_songs', isLoggedIn, function(req, res) {
-		console.log("'"+req.user.username+"'");
+		//console.log("'"+req.user.username+"'");
         var query = req.body.query,
 		s_id = req.body.tag;
 		// name = req.body.name;
-		console.log("'"+temp1+"'");
-		console.log(s_id);
+		//console.log("'"+temp1+"'");
+		//console.log(s_id);
 		var item={user_id: req.user.username,playlist_name:temp1}
 		var item_insert={song_id: s_id}
 
 
 		mongodb.connect(database_mongo.url,function(err, db){
-		console.log('Insertion started');
+		//console.log('Insertion started');
 		assert.equal(null,err);
 		// db.collection('Collection_user').update({user_id: "'"+req.user.username+"'", playlist_name: "'"+temp1+"'"}, {$push: {song_id: s_id}}, function(err, result){
 		// // assert.equal(null,error);
 		// // console.log('Item inserted');
 		// // db.close();
 		// });
-
-		db.collection('Collection_user').update({user_id: req.user.username, playlist_name: temp1}, {$push: {song_id: s_id}});
+		db.collection('Collection_user').update({user_id: req.user.username, playlist_name: temp1}, {$push: {song_id: s_id}}, {upsert:true});
 
 		res.redirect('/add_songs?tag='+temp1);
 		});
@@ -398,14 +486,14 @@ module.exports = function(app, passport) {
 		s_id = req.body.tag1;
 		// // name = req.body.name;
 		// console.log("'"+temp1+"'");
-		console.log("Here");
-		console.log(s_id);
+		//console.log("Here");
+		//console.log(s_id);
 		// var item={user_id: req.user.username,playlist_name:temp1}
 		// var item_insert={song_id: s_id}
 
 
 		mongodb.connect(database_mongo.url,function(err, db){
-		console.log('Insertion started');
+		//console.log('Insertion started');
 		assert.equal(null,err);
 		// // db.collection('Collection_user').update({user_id: "'"+req.user.username+"'", playlist_name: "'"+temp1+"'"}, {$push: {song_id: s_id}}, function(err, result){
 		// // // assert.equal(null,error);
@@ -433,7 +521,7 @@ module.exports = function(app, passport) {
 		assert.equal(null,err);
 		db.collection('Collection_songs').insertOne(item, function(err, result){
 		// assert.equal(null,error);
-		console.log('Item inserted');
+		//console.log('Item inserted');
 		db.close();
 		});
 		});
@@ -460,6 +548,14 @@ function isLoggedIn(req, res, next) {
 		return next();
 
 	res.redirect('/');
+}
+
+function isNotLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		res.redirect('/profile');
+
+	else
+		return next();
 }
 
 function sleep(ms){
