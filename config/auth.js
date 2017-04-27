4e729dc9fec2e8fe5b4ab50158d35d78e3bd29f3
swathirajanna <rswat@seas.upenn.edu>
2017-04-27 13:32:42 -0400

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -3,12 +3,12 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1364406843619494',
         'clientSecret'  : '020a22d2300f5c97a8fdb52f769a9473',
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://54.202.206.114:8080/auth/facebook/callback'
     },
 
     'spotifyAuth' : {
         'clientID'       : '2f52909fe5414c4b915085f3b7eceece',
         'clientSecret'    : 'eacd75b3b36845c5b6bcc6a65f09bfe9',
-        'callbackURL'       : 'http://localhost:8080/auth/spotify/callback'
+        'callbackURL'       : 'http://54.202.206.114:8080/auth/spotify/callback'
     }
 };
